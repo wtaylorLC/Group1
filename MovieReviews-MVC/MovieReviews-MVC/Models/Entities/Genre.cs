132c0937b7a0cc927f3104811055a8833ddf82a2
Spell2az <martin_kovac@outlook.com>
2019-03-28 11:43:05 +0000

    Add Genre model

---
+++ MovieReviews-MVC/MovieReviews-MVC/Models/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MovieReviews_MVC.Models.Entities
+{
+  public class Genre
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public ICollection<Movie> Movies { get; set; }
+  }
+}
