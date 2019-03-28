d4d19954cc8e3005f3894851f1a9da125413571d
Spell2az <martin_kovac@outlook.com>
2019-03-28 11:43:21 +0000

    Add Movie model

---
+++ MovieReviews-MVC/MovieReviews-MVC/Models/Entities/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MovieReviews_MVC.Models.Entities
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public int Length { get; set; }
+    public string Image { get; set; }
+    public float Rating { get; set; }
+
+
+
+    // Navigation Properties
+
+    // FilmCrew
+    public virtual ICollection<Genre> Genres { get; set; }
+    // Revies
+    // Ratings
+  }
+}
