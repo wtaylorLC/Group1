cf4a931ffd96778e16f5283663f383bc25e72f1e
Spell2az <martin_kovac@outlook.com>
2019-04-01 05:15:49 +0100

    Remove pluralizing table name convetion

--- MovieReviews-MVC/MovieReviews-MVC/Models/IdentityModels.cs
+++ MovieReviews-MVC/MovieReviews-MVC/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -8,39 +9,43 @@ using MovieReviews_MVC.Models.Entities;
 
 namespace MovieReviews_MVC.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+  // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+  public class ApplicationUser : IdentityUser
+  {
+
+
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
+      // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+      var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+      // Add custom user claims here
+      return userIdentity;
     }
+  }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-      protected override void OnModelCreating(DbModelBuilder modelBuilder)
-      {
-        modelBuilder.Configurations.Add(new GenreEntityDbConfiguration());
-        modelBuilder.Configurations.Add(new MovieEntityDbConfiguration());
-
-        base.OnModelCreating(modelBuilder);
     }
 
-      public DbSet<Genre> Genres { get; set; }
-      public DbSet<Movie> Movies { get; set; }
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
     }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+      
+      modelBuilder.Configurations.Add(new GenreEntityDbConfiguration());
+      modelBuilder.Configurations.Add(new MovieEntityDbConfiguration());
+
+      base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+  }
 }
