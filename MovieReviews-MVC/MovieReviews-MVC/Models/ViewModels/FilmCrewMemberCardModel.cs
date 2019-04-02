1ed99dfa7076c278d551742a322ad2b6e24f5dbc
Spell2az <martin_kovac@outlook.com>
2019-04-02 09:21:46 +0100

    Add view model for filmcrew member cards

---
+++ MovieReviews-MVC/MovieReviews-MVC/Models/ViewModels/FilmCrewMemberCardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieReviews_MVC.Models.ViewModels
+{
+  public class FilmCrewMemberCardModel
+  {
+    public int Id { get; set; }
+    public string ImageUri { get; set; }
+    public string Name { get; set; }
+  }
+}
