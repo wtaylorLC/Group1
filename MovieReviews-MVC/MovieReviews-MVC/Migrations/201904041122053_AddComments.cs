1ad7c5e5c7d98f243a29e563118283a42e90da39
Spell2az <martin_kovac@outlook.com>
2019-04-04 12:32:55 +0100

    Add comment migration

---
+++ MovieReviews-MVC/MovieReviews-MVC/Migrations/201904041122053_AddComments.cs
@@ -0,0 +1,30 @@
+namespace MovieReviews_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddComments : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Comment",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AuthorId = c.String(),
+                        Body = c.String(),
+                        CreatedOn = c.DateTime(nullable: false),
+                        CommentParentId = c.Int(nullable: true),
+                        CommentSubjectId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Comment");
+        }
+    }
+}
