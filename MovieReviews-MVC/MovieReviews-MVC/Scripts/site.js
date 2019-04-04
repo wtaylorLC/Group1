edbf764d1b916d370923b59f1499dfcf351244f9
Spell2az <martin_kovac@outlook.com>
2019-04-04 16:53:57 +0100

    Fade in for card jquery

--- MovieReviews-MVC/MovieReviews-MVC/Scripts/site.js
+++ MovieReviews-MVC/MovieReviews-MVC/Scripts/site.js
@@ -1,23 +1,17 @@
-﻿
+﻿var site = site || {};
+site.baseUrl = site.baseUrl || "";
 
-$(document).ready(function(e) {
+$(document).ready( (e) => {
 
   // locate each partial section.
   // if it has a URL set, load the contents into the area.
 
-  $(".partial-content").each(function(index, item) {
-    //var url = site.baseUrl + $(item).data("url");
-    //if (url && url.length > 0) {
-    $(item).load($(item).data("url"), () => {
-      $(".card").delay(100).hide().slideDown(200);
-    });
-
-    //.slideDown('slow')
-    //.animate(
-    //  { opacity: 1 },
-    //  { queue: false, duration: 'slow' }
-    //);
-    //}
+  $(".partials-content").each(function(index, item) {
+    var url = site.baseUrl + $(item).data("url");
+    if (url && url.length > 0) {
+      $(item).load(url, () => {
+        $(".card").hide().delay(100).fadeIn("600");
+      });
+    }
   });
-
-});
+})
