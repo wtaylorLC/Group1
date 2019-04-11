ece6116fdcbca25b61269ee1ff93996e9bf14848
Spell2az <martin_kovac@outlook.com>
2019-04-11 12:39:13 +0100

    Remove report and comment buttons for unauthenticated users, Add comments to all Posts

---
+++ MovieReviews-MVC/MovieReviews-MVC/Models/ViewModels/FillmCrewMemberViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MovieReviews_MVC.Models.Entities;
+
+namespace MovieReviews_MVC.Models.ViewModels
+{
+    public class FillmCrewMemberViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        [Display(Name = "Date of Birth:")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime DoB { get; set; }
+        public string ImageUri { get; set; }
+        public MovieRole Role { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
